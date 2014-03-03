46a14074240fd3fedab9a9bd3e2ea20388a76cd2
LiGang <ligang@LiGangdeMacBook-Pro.local>
2014-03-03 17:07:42 +0800

    2BGSX

---
+++ GitTest/GitTest/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  GitTest
+//
+//  Created by LiGang on 14-3-3.
+//  Copyright (c) 2014年 LiGang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "LGAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([LGAppDelegate class]));
+    }
+}
